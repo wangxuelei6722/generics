00438d98d1598e65c1c5eafed9b6bebcaf4abcc4
Administrator <15110096722@163.com>
2020-11-16 15:38:11 +0800

    初次提交

---
+++ src/main/java/com/wangxl/generics/GenericsApplication.java
@@ -0,0 +1,13 @@
+package com.wangxl.generics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangxl/generics/GenericsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangxl.generics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
