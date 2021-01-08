6afbffaacfe983037722a842ad1ff7b6710afa98
Administrator <15110096722@163.com>
2021-01-08 16:36:19 +0800

    泛型接口

--- src/main/java/com/wangxl/generics/coffee/Fibonacci7.java
+++ src/main/java/com/wangxl/generics/coffee/Fibonacci7.java
@@ -18,15 +18,44 @@ public class Fibonacci7 implements Generator<Integer>,Iterable<Integer> {
         return fib(n-2) + fib(n-1);
 
     }
+    public Fibonacci7(){}
+    public Fibonacci7(int m){
+        this.m = m;
+    }
 
     @Override
     public Integer next() {
-        return null;
+        return fib(count++);
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return m > 0;
+            }
+
+            @Override
+            public Integer next() {
+                m--;
+                return Fibonacci7.this.next();
+            }
+            public void remove(){
+                throw new UnsupportedOperationException();
+            }
+
+        };
     }
+    public static void main(String[] args) {
+        Fibonacci7 gen = new Fibonacci7();
+        for(int i = 0; i < 18; i++)
+            System.out.print(gen.next() + " ");
+        System.out.println();
+        Iterator it = new Fibonacci7(20).iterator();
+        while(it.hasNext())
+            System.out.print(it.next() + " ");
+    }
+
 
 }
